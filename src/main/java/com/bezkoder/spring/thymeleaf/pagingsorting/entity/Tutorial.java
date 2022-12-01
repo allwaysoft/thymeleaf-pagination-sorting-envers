e909fc95e3307424a8f904fecb4a69d5eea668d0
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2022-12-01 11:29:26 +0800

    Add files via upload

--- src/main/java/com/bezkoder/spring/thymeleaf/pagingsorting/entity/Tutorial.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/pagingsorting/entity/Tutorial.java
@@ -1,8 +1,15 @@
 package com.bezkoder.spring.thymeleaf.pagingsorting.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 import org.hibernate.envers.Audited;
 
+@Data
 @Entity
 @Table(name = "tutorials")
 @Audited
@@ -19,61 +26,10 @@ public class Tutorial {
     private String description;
 
     @Column(nullable = false)
-    private int level;
+    private Integer level;
 
     @Column
-    private boolean published;
-
-    public Tutorial() {
-
-    }
-
-    public Tutorial(String title, String description, int level, boolean published) {
-        this.title = title;
-        this.description = description;
-        this.level = level;
-        this.published = published;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public boolean isPublished() {
-        return published;
-    }
-
-    public void setPublished(boolean published) {
-        this.published = published;
-    }
+    private Boolean published;
 
     @Override
     public String toString() {
--- src/main/java/com/bezkoder/spring/thymeleaf/pagingsorting/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/pagingsorting/repository/TutorialRepository.java
@@ -1,7 +1,5 @@
 package com.bezkoder.spring.thymeleaf.pagingsorting.repository;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bezkoder.spring.thymeleaf.pagingsorting.entity.Tutorial;
+import jakarta.transaction.Transactional;
 import org.springframework.data.repository.history.RevisionRepository;
 
 @Repository
